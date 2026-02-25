1cfad7e58bd51cdd22005d9068cc29a3cc48c539
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-02-26 03:51:03 +0500

    OrderRepository reimplemented.

--- OrderManager.API/Repositories/IOrderRepository.cs
+++ OrderManager.API/Repositories/IOrderRepository.cs
@@ -1,13 +1,9 @@
-﻿using OrderManager.Entities;
+using OrderManager.Entities;
 
-namespace OrderManager.API.Repositories
+namespace OrderManager.API.Repositories;
+
+public interface IOrderRepository : IGenericRepository<Order>
 {
-	public interface IOrderRepository
-	{
-		Task<IEnumerable<Order>> GetAllOrdersAsync(bool includeOrderLines = false);
-		Task<Order?> GetOrderByIdAsync(int orderId, bool includeOrderLines = false);
-		Task<Order> CreateOrderAsync(Order order);
-		Task UpdateOrderAsync(Order order);
-		Task DeteteOrderAsync(int orderId);
-	}
+    Task<IEnumerable<Order>> GetAllOrdersAsync(bool includeOrderLines = false);
+    Task<Order?> GetOrderByIdAsync(int orderId, bool includeOrderLines = false);
 }
--- OrderManager.API/Repositories/OrderRepository.cs
+++ OrderManager.API/Repositories/OrderRepository.cs
@@ -1,63 +1,36 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using OrderManager.DbContexts;
 using OrderManager.Entities;
 
-namespace OrderManager.API.Repositories
+namespace OrderManager.API.Repositories; 
+
+public class OrderRepository : GenericRepository<Order>, IOrderRepository
 {
-	public class OrderRepository : IOrderRepository
-	{
-		private readonly OrderManagerDbContext _context;
+    public OrderRepository(OrderManagerDbContext context) : base(context)
+    {
+    }
 
-		public OrderRepository(OrderManagerDbContext context)
-		{
-			_context = context;
-		}
+    public async Task<IEnumerable<Order>> GetAllOrdersAsync(bool includeOrderLines = false)
+    {
+        if (includeOrderLines)
+        {
+            return await _context.Orders.Include(o => o.OrderLines).ToListAsync();
+        }
+        else
+        {
+            return await _context.Orders.ToListAsync();
+        }
+    }
 
-		public async Task<IEnumerable<Order>> GetAllOrdersAsync(bool includeOrderLines = false)
-		{
-			if (includeOrderLines)
-			{
-				return await _context.Orders.Include(o => o.OrderLines).ToListAsync();
-			}
-			else
-			{
-				return await _context.Orders.ToListAsync();
-			}
-		}
-
-		public async Task<Order?> GetOrderByIdAsync(int orderId, bool includeOrderLines = false)
-		{
-			if (includeOrderLines)
-			{
-				return await _context.Orders.Include(o => o.OrderLines).FirstOrDefaultAsync(o => o.Id == orderId);
-			}
-			else
-			{
-				return await _context.Orders.FirstOrDefaultAsync(o => o.Id == orderId);
-			}
-		}
-
-		public async Task<Order> CreateOrderAsync(Order order)
-		{
-			_context.Orders.Add(order);
-			await _context.SaveChangesAsync() ;
-			return order;
-		}
-
-		public async Task UpdateOrderAsync(Order order)
-		{
-			_context.Orders.Update(order);
-			await _context.SaveChangesAsync();
-		}
-
-		public async Task DeteteOrderAsync(int orderId)
-		{
-			var order = await _context.Orders.FindAsync(orderId);
-			if (order is not null)
-			{
-				_context.Orders.Remove(order);
-				await _context.SaveChangesAsync();
-			}
-		}
-	}
+    public async Task<Order?> GetOrderByIdAsync(int orderId, bool includeOrderLines = false)
+    {
+        if (includeOrderLines)
+        {
+            return await _context.Orders.Include(o => o.OrderLines).FirstOrDefaultAsync(o => o.Id == orderId);
+        }
+        else
+        {
+            return await _context.Orders.FirstOrDefaultAsync(o => o.Id == orderId);
+        }
+    }
 }
