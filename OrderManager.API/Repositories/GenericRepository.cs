6a25445039924d667aae078961ad87a774b037b8
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-02-25 08:15:53 +0500

    GenericRepository interface implementation added.

---
+++ OrderManager.API/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderManager.DbContexts;
+
+namespace OrderManager.API.Repositories
+{
+	public class GenericRepository<T> : IGenericRepository<T> where T : class
+	{
+		protected readonly OrderManagerDbContext _context;
+
+		public GenericRepository(OrderManagerDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<T> AddAsync(T entity)
+		{
+			await _context.Set<T>().AddAsync(entity);
+			return entity;
+		}
+
+		public async Task<T?> GetByIdAsync(int id)
+		{
+			return await _context.Set<T>().FindAsync(id);
+		}
+
+		public async Task<IEnumerable<T>> GetAllAsync()
+		{
+			return await _context.Set<T>().ToListAsync();
+		}
+
+		public Task UpdateAsync(T entity)
+		{
+			_context.Set<T>().Update(entity);
+			return Task.CompletedTask;
+		}
+
+		public async Task DeleteAsync(int id)
+		{
+			var entity = await _context.Set<T>().FindAsync(id);
+			if (entity != null)
+			{
+				_context.Set<T>().Remove(entity);
+			}
+		}
+	}
+}
