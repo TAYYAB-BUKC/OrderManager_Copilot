58c7e897a86c8b4ead55b313b4ddfa4bcfcc48fb
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-02-26 03:40:51 +0500

    CreateOrderWithOrderLinesUnitOfWork reimplementated.

--- OrderManager.API/UnitOfWork/CreateOrderWithOrderLinesUnitOfWork.cs
+++ OrderManager.API/UnitOfWork/CreateOrderWithOrderLinesUnitOfWork.cs
@@ -1,49 +1,51 @@
-﻿using OrderManager.API.Repositories;
+using Microsoft.EntityFrameworkCore;
+using OrderManager.API.Repositories;
 using OrderManager.DbContexts;
 using OrderManager.Entities;
+using System;
+using System.Threading.Tasks;
 
-namespace OrderManager.API.UnitOfWork
+namespace OrderManager.API.UnitsOfWork;
+
+public class CreateOrderWithOrderLinesUnitOfWork : IUnitOfWork
 {
-	public class CreateOrderWithOrderLinesUnitOfWork : IUnitOfWork
-	{
-		private readonly OrderManagerDbContext _context;
-		private readonly IOrderRepository _orderRepository;
-		private readonly IOrderLineRepository _orderLineRepository;
+    private readonly OrderManagerDbContext _context;
+    private readonly IGenericRepository<Order> _orderRepository;
+    private readonly IGenericRepository<OrderLine> _orderLineRepository; 
 
-		public CreateOrderWithOrderLinesUnitOfWork(OrderManagerDbContext context,
-			IOrderRepository orderRepository,
-			IOrderLineRepository orderLineRepository)
-		{
-			_context = context;
-			_orderRepository = orderRepository;
-			_orderLineRepository = orderLineRepository;
-		}
+    public CreateOrderWithOrderLinesUnitOfWork(OrderManagerDbContext context,
+        IGenericRepository<Order> orderRepository,
+        IGenericRepository<OrderLine> orderLineRepository)
+    {
+        _context = context;
+        _orderRepository = orderRepository;
+        _orderLineRepository = orderLineRepository;
+    }
 
-		public async Task<int> SaveChangesAsync()
-		{
-			return await _context.SaveChangesAsync();
-		}
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
 
-		public async Task RollbackAsync()
-		{
-			_context.ChangeTracker.Clear();
-			await Task.CompletedTask;
-		}
+    public async Task RollbackAsync()
+    {
+        _context.ChangeTracker.Clear();
+        await Task.CompletedTask;
+    }
 
-		public async Task CreateOrderWithOrderLinesAsync(Order order,
-			IEnumerable<OrderLine> orderLines)
-		{
-			await _orderRepository.CreateOrderAsync(order);
-			foreach (var line in orderLines)
-			{
-				line.Order = order;
-				await _orderLineRepository.CreateOrderLineAsync(line);
-			}
-		}
+    public async Task CreateOrderWithOrderLinesAsync(Order order,
+        IEnumerable<OrderLine> orderLines)
+    {
+        await _orderRepository.AddAsync(order);
+        foreach (var line in orderLines)
+        {
+            line.Order = order;
+            await _orderLineRepository.AddAsync(line);
+        }
+    }
 
-		public void Dispose()
-		{
-			// nothing to see here
-		}
-	}
+    public void Dispose()
+    {
+        // nothing to see here
+    }
 }
