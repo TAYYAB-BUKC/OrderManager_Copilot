2b4268fa67940f819f8a4ca063dbba8b8cd0282a
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-02-25 06:39:50 +0500

    GenericRepository interface added.

---
+++ OrderManager.API/Repositories/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderManager.API.Repositories
+{
+	public interface IGenericRepository<T> where T : class
+	{
+		Task<T> AddAsync(T entity);
+		Task<T?> GetByIdAsync(int id);
+		Task<IEnumerable<T>> GetAllAsync();
+		Task UpdateAsync(T entity);
+		Task DeleteAsync(int id);
+	}
+}
